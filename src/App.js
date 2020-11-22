c5d41f986927bcb8d9d07429b6fc0d961e7ebfa7
Finnschy <skylerfearless@yahoo.de>
2020-11-22 16:22:30 +0100

    fertig

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FirstSection from './components/FirstSection';
 import Header from './components/Header';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <main>
         <Header />
         <FirstSection />
+        <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+const Footer = () => {
+    return ( 
+        <footer id="footerGrid">
+            <figure>
+                <img src="assets/img/salat.jpeg" alt=""/>
+                <figcaption>Headline 1</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$45 / $55</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/pizza.jpeg" alt=""/>
+                <figcaption>Headline 2</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$65 / $70</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/frikadellen.jpeg" alt=""/>
+                <figcaption>Headline 3</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$30.50</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/glass.jpeg" alt=""/>
+                <figcaption>Headline 4</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$25.50</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/pizza2.jpeg" alt=""/>
+                <figcaption>Headline 5</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$25.50</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/scampi.jpeg" alt=""/>
+                <figcaption>Headline 6</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$25.50</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/spagetthi.jpeg" alt=""/>
+                <figcaption>Headline 7</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$25.50</h6>
+            </figure>
+            <figure>
+                <img src="assets/img/teller.jpeg" alt=""/>
+                <figcaption>Headline 8</figcaption>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+                <h6 className="pricetag">$25.50</h6>
+            </figure>
+        </footer>
+     );
+}
+ 
+export default Footer;
